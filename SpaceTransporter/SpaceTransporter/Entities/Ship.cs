9c1b8ae72b180a3c2bda296c226a4ddbb792c7f7
Jan Slavik <janslavikgreenfox@gmail.com>
2020-11-13 09:32:28 +0100

    SpaceTransporter - add Ship, Plantet and static initial frontend page

---
+++ SpaceTransporter/SpaceTransporter/Entities/Ship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceTransporter.Entities
+{
+    public class Ship
+    {
+        public int Id { get; set; }
+        public double MaxWarpSpeed { get; set; }
+
+        public bool IsDocked { get; set; }
+        public int Name { get; set; }
+    }
+}
